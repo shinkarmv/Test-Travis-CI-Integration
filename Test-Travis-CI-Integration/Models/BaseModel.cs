3d56b95676528d4a080db8d85da6721f9b96b0c6
shinkarmv <31374791+shinkarmv@users.noreply.github.com>
2017-11-12 13:14:34 +0530

    Added project

---
+++ Test-Travis-CI-Integration/Models/BaseModel.cs
@@ -0,0 +1,28 @@
+﻿using Jil;
+
+namespace Test_Travis_CI_Integration.Models
+{
+    class BaseModel
+    {
+        [JilDirective(Name = "id")]
+        public string Id { get; set; }
+
+        [JilDirective(Name = "country")]
+        public string Country { get; set; }
+
+        [JilDirective(Name = "name")]
+        public string Name { get; set; } 
+
+        [JilDirective(Name = "abbr")]
+        public string Abbreviation { get; set; }
+
+        [JilDirective(Name = "area")]
+        public string Area { get; set; }
+
+        [JilDirective(Name = "largest_city")]
+        public string LargestCity { get; set; }
+
+        [JilDirective(Name = "capital")]
+        public string Capital { get; set; }
+    }
+}
