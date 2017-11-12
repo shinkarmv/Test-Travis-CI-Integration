24892b95b2c3a2d1600ae4a7382fe6b876dad109
shinkarmv <31374791+shinkarmv@users.noreply.github.com>
2017-11-12 12:26:46 +0530

    Added test project

---
+++ GetRecords.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using Test_Travis_CI_Integration.Models;
+
+namespace Test_Travis_CI_Integration
+{
+    static class GetRecords
+    {
+        internal static Response GetCountryDetails(string countryCode)
+        {
+            var url = ConfigurationManager.AppSettings["apiUrl"] + countryCode + "/all";
+            var result = WebCaller.Get(url, "", "");
+            Response value = Newtonsoft.Json.JsonConvert.DeserializeObject<Response>(result, WebCaller.GetSettings());
+            return value;
+        }
+
+        internal static Response GetSateDetailsOnCountry(string countryCode, string stateCode)
+        {
+            var url = ConfigurationManager.AppSettings["apiUrl"] + countryCode + stateCode;
+            var result = WebCaller.Get(url, "", "");
+            Response value = Newtonsoft.Json.JsonConvert.DeserializeObject<Response>(result, WebCaller.GetSettings());
+            //JSON.Deserialize<Response>(result, WebCaller.GetSettings());
+            return value;
+        }
+    }
+}
